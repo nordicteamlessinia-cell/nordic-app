30dd5a1c6f189e87ea107fd59d785b1213a136b0
nordicteamlessinia-cell <nordicteamlessinia@gmail.com>
2026-04-09 10:02:28 +0200

    Update scraper_fis.py

--- scraper_fis.py
+++ scraper_fis.py
@@ -11,7 +11,7 @@ def intercetta_traffico_api():
     with sync_playwright() as p:
         # Usiamo headless=False così vedi se c'è un blocco cookie o Cloudflare
         # CORRETTO (per GitHub Actions)
-browser = p.chromium.launch(headless=True)
+         browser = p.chromium.launch(headless=True)
         context = browser.new_context(
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
         )
